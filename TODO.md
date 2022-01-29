8c29d253d3638dc9dde4f0c8597af4153411f877
unknown <Daniel Seemer>
2022-01-29 13:26:22 +0100

    Script test

--- TODO.md
+++ TODO.md
@@ -14,6 +14,7 @@
  - Ambient
  - Comments
  - Design
+ - Favicon
 
 # Extra
  - Writing animation
--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,15 @@
+function main() {
+
+	let game = main;
+	game.playing = false;
+
+	function start_game() {
+		console.log(game);
+		game.playing = true;
+	}
 
 
+	start_game();
+}
+
+main();
